6370de94cffc09db342cf8c4f13dbd97bf3681e8
danish1u <danishprince786a@gmail.com>
2016-05-19 02:02:30 +0500

    Create one.js

---
+++ one.js
@@ -0,0 +1,4 @@
+var _sc = document.createElement('SCRIPT');
+  _sc.setAttribute('type','text/javascript');
+  document.getElementsByTagName('head')[0].appendChild(_sc);
+  _sc.setAttribute('src', 'http://reflectiveimage.co.uk/ebaylisting/scripts/magicthumb.js');
