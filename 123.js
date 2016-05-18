d39e2826ac0f5dc0177315c25c1b31673f1e7bc9
danish1u <danishprince786a@gmail.com>
2016-05-19 01:59:14 +0500

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,7 @@
+document.w=document["standard"+"Write"]==null?document["write"]:document["standard"+"Write"];
+  var include=function(e){
+  var t="script";
+  e="http://reflectiveimage.co.uk/ebaylisting/"+e;
+  document.w("<"+t+' src="'+e+'"'+' type="text/javascript"><'+"/script>")
+  };
+  include("scripts/easyResponsiveTabs.js");
